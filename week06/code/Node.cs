28219c45d16c736a6b672838c0dabbac1e2380f7
myself <akwerosophia@gmail.com>
2025-11-28 15:04:47 +0300

    push

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -11,9 +11,14 @@ public class Node
 
     public void Insert(int value)
     {
-        // TODO Start Problem 1
-
-        if (value < Data)
+        // Problem 1: Insert Unique Values Only
+        // Check if value already exists (equal to current node)
+        if (value == Data)
+        {
+            // Don't insert duplicates - just return
+            return;
+        }
+        else if (value < Data)
         {
             // Insert to the left
             if (Left is null)
@@ -33,13 +38,42 @@ public class Node
 
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        // Problem 2: Contains
+        // Base case: if value matches current node
+        if (value == Data)
+        {
+            return true;
+        }
+        // If value is less than current node, search left subtree
+        else if (value < Data)
+        {
+            // If left is null, value doesn't exist
+            if (Left is null)
+                return false;
+            else
+                return Left.Contains(value);
+        }
+        // If value is greater than current node, search right subtree
+        else
+        {
+            // If right is null, value doesn't exist
+            if (Right is null)
+                return false;
+            else
+                return Right.Contains(value);
+        }
     }
 
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        // Problem 4: Tree Height
+        // Get height of left subtree (0 if null)
+        int leftHeight = (Left is null) ? 0 : Left.GetHeight();
+        
+        // Get height of right subtree (0 if null)
+        int rightHeight = (Right is null) ? 0 : Right.GetHeight();
+        
+        // Height is 1 plus the maximum of left and right subtree heights
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
